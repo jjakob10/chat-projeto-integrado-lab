66abd6d5d74ad06b341d587c521509925a9973b2
jjakob10 <johann.bastos@edu.ufes.br>
2024-04-25 14:27:42 -0300

    first commit

---
+++ src/main/java/com/ufes/pi/chat/OChatDaAulaDeProjetoIntegradoApplication.java
@@ -0,0 +1,13 @@
+package com.ufes.pi.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OChatDaAulaDeProjetoIntegradoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OChatDaAulaDeProjetoIntegradoApplication.class, args);
+	}
+
+}
